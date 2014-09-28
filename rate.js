01a71bdeb9a67d797d7005705cbc10f2499e6707
jomkin <jomkin@qq.com>
2014-09-28 13:44:47 +0800

    Update rate.js

--- rate.js
+++ rate.js
@@ -1,21 +1,21 @@
 rateFunction()
 function rateFunction(){
        
-var cfg = {
-        usesUntilPrompt: 2,
-        displayAppName: "My Super App",
-        customLocale: {
-            title: "Rate %@",
-            message: "If you enjoy using %@, would you mind taking a moment to rate it? It won’t take more than a minute. Thanks for your support!",
-            buttonLabels: ["No, Thanks", "Remind Me Later", "Rate It Now",]
-        },
-        appStoreAppURL: {
-            ios: 'itms-apps://itunes.apple.com/ru/app/id736199575?l=en&mt=8'
-        }
-    };
+//var cfg = {
+//        usesUntilPrompt: 8,
+//        displayAppName: "My Super App",
+//        customLocale: {
+//            title: "Rate %@",
+ //           message: "If you enjoy using %@, would you mind taking a moment to rate it? It won’t take more than a minute. Thanks for your support!",
+ //           buttonLabels: ["No, Thanks", "Remind Me Later", "Rate It Now",]
+ //       },
+  //      appStoreAppURL: {
+ //           ios: 'itms-apps://itunes.apple.com/ru/app/id736199575?l=en&mt=8'
+  //      }
+  //  };
 
-    navigator.apprate.setup(cfg);
-    navigator.apprate.promptForRating();
+ //   navigator.apprate.setup(cfg);
+ //   navigator.apprate.promptForRating();
    
 }
 
